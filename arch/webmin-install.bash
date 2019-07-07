61feb1fe8fd358596bb1031f426d381b7725d903
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-07 14:55:41 -0300

    Create webmin-install.bash

---
+++ arch/webmin-install.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+myusr="myuser"
+
+su $myusr
+cd ~
+mkdir builds
+cd builds
+
+sudo pacman -S --needed base-devel
+
+git clone https://aur.archlinux.org/perl-authen-pam.git
+cd perl-authen-pam
+makepkg -s --noconfirm
+sudo pacman -U --noconfirm ./*.pkg.tar.xz
+cd ..
+
+git clone https://aur.archlinux.org/perl-encode-detect.git
+cd perl-encode-detect
+makepkg -s --noconfirm
+sudo pacman -U --noconfirm ./*.pkg.tar.xz
+cd ..
+
+git clone https://aur.archlinux.org/webmin.git
+cd webmin
+makepkg -s --noconfirm
+sudo pacman -U --noconfirm ./*.pkg.tar.xz
+cd ..
+
+sudo systemctl --now enable webmin
+
+#https://<server>:10000
