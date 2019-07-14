a2efe482a1899905d908f9a20a97be530d764d1e
VLS <vini_ls@hotmail.com>
2019-07-14 03:34:25 -0300

    uploading mirrorlist

---
+++ arch/mirrorlist.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+country="BR"
+destinationFile="/etc/pacman.d/mirrorlist"
+rm /etc/pacman.d/mirrorlist
+curl "https://www.archlinux.org/mirrorlist/?country=$country&protocol=http&protocol=https&ip_version=4" -o $destinationFile
+sed -i 's/#S/S/g' $destinationFile
