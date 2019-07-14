969a59766b9df6a2346d49dfe680ca9a68c285ec
VLS <vini_ls@hotmail.com>
2019-07-14 04:15:41 -0300

    problem to execute

--- arch/mirrorlist.sh
+++ arch/mirrorlist.sh
@@ -1,6 +1,6 @@
-﻿#!/bin/bash
+#!/bin/bash
 
-sudo rm /etc/pacman.d/mirrorlist
-sudo curl "https://www.archlinux.org/mirrorlist/?country=BR&protocol=http&protocol=https&ip_version=4" -o mirrorlist
+rm /etc/pacman.d/mirrorlist
+curl "https://www.archlinux.org/mirrorlist/?country=BR&protocol=http&protocol=https&ip_version=4" -o mirrorlist
 sudo sed -i 's/#S/S/g' mirrorlist
 sudo mv mirrorlist /etc/pacman.d/mirrorlist
