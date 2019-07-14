86a3f41edbff904af6efc8d5809d4d4c2fd9fbb9
VLS <vini_ls@hotmail.com>
2019-07-14 03:56:07 -0300

    asdf

--- arch/mirrorlist.sh
+++ arch/mirrorlist.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
-country="BR"
-destinationFile="/etc/pacman.d/mirrorlist"
-rm /etc/pacman.d/mirrorlist
-curl "https://www.archlinux.org/mirrorlist/?country=$country&protocol=http&protocol=https&ip_version=4" -o $destinationFile
-sed -i 's/#S/S/g' $destinationFile
+
+sudo rm /etc/pacman.d/mirrorlist
+sudo curl "https://www.archlinux.org/mirrorlist/?country=BR&protocol=http&protocol=https&ip_version=4" -o mirrorlist
+sudo sed -i 's/#S/S/g' mirrorlist
+sudo mv mirrorlist /etc/pacman.d/mirrorlist
