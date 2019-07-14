552a5c308a5a37ea57286843cf692830cee4206a
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-14 11:33:25 -0300

    Update mirrorlist.sh

--- arch/mirrorlist.sh
+++ arch/mirrorlist.sh
@@ -4,6 +4,7 @@ rm /etc/pacman.d/mirrorlist
 curl "https://www.archlinux.org/mirrorlist/?country=BR&protocol=http&protocol=https&ip_version=4" -o mirrorlist
 sudo sed -i 's/#S/S/g' mirrorlist
 sudo mv mirrorlist /etc/pacman.d/mirrorlist
+sudo pacman -Syy
 
 #sudo pacman -S pacman-contrib
 #sudo curl -o ~/mirrorlist https://www.archlinux.org/mirrorlist/all/
