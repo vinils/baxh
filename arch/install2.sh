443d207dd362df0a544fb388185cf36ffb2687e2
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-17 01:44:58 -0300

    Update install2.sh

--- arch/install2.sh
+++ arch/install2.sh
@@ -69,7 +69,7 @@ grub-install --target=x86_64-efi --efi-directory=/"$bootDir" --recheck
 grub-install --target=x86_64-efi --efi-directory=/"$bootDir" --bootloader-id=arch_gru
 
 #(if intell virtualization VT-x)
-sed -i 's/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"intel_iommu=on/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"intel_iommu=on\ iommu=pt/g' /etc/default/grub
 
 grub-mkconfig -o /"$bootDir"/grub/grub.cfg
 
