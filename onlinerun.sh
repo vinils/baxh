0d5e24078b85e3164896f5fa8ccf50b38694c1ac
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-18 20:08:57 -0300

    Update and rename onlinerun.sh to onlinerun

--- onlinerun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir /var/onlinerun/
-randomStr=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
-filePath="/var/onlinerun/tmp$randomStr.bash"
-curl $1 -o $filePath
-chmod +xr $filePath
-./$filePath $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 
-rm $filePath
