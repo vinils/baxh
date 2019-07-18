6a64d9d30b1adfd254bfa80139f6c8283b4f5b71
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-18 08:26:14 -0300

    Update mirrorlist.sh

--- arch/mirrorlist.sh
+++ arch/mirrorlist.sh
@@ -11,3 +11,11 @@ sudo pacman -Syy
 #sudo rankmirrors ~/mirrorlist > ~/mirrorlist.fastest
 #sudo mv -v ~/mirrorlist.fastest /etc/pacman.d/mirrorlist
 #sudo pacman -Syy
+
+#sudo pacman -S reflector
+##Verbosely rate and sort the five most recently synchronized mirrors by download speed, and overwrite the file /etc/pacman.d/mirrorlist:
+#sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+##Select the 200 most recently synchronized HTTP or HTTPS mirrors, sort them by download speed, and overwrite the file /etc/pacman.d/mirrorlist:
+#sudo reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+##Select the HTTPS mirrors synchronized within the last 12 hours and located in the US, sort them by download speed, and overwrite the file /etc/pacman.d/mirrorlist:
+#sudo reflector --country 'United States' --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
