9cb547ed354cb674036fc8e5d8e34f89d616bf05
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-28 00:13:47 -0300

    Update Drivers.ps1

--- windows/Drivers.ps1
+++ windows/Drivers.ps1
@@ -28,9 +28,12 @@ pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\Intel_LAN(v23.
 
 pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\Realtek_Audio(v8051_FF10)\WIN64\*.inf"
 
+".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\chipset-10.1.17\SetupChipset.exe" -s -norestart
+#pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\chipset-10.1.17\DriverFiles\production\W2K12R2-x64\*.inf"
+pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\chipset-10.1.17\DriverFiles\production\Windows10-x64\*.inf"
+pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\chipset-10.1.17\DriverFiles\production\W2K16-x64\*.inf"
+#pnputil -i -a ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\chipset-10.1.17\DriverFiles\production\Windows10-x86\*.inf"
 ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\INF(v10.1.1.44_Public)\SetupChipset.exe" -s -norestart
 ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\Chipset_Win10_10.1.1.44\SetupChipset.exe" -s -norestart
-chipset-10.1.17
-
 
 ".\SOFTWARES\SETUP\X299\2016 server\DRIVERS\NetWork\AMD-Catalyst-15.7.1-Win10-64bit\Bin64\ATISetup.exe" -Install -output screen
