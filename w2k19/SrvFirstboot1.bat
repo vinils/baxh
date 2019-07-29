259838bc5eabd083b772c4117dd7f328aa366544
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-28 23:11:05 -0300

    Update SrvFirstboot1.bat

--- w2k19/SrvFirstboot1.bat
+++ w2k19/SrvFirstboot1.bat
@@ -1,5 +1,19 @@
 netdom renamecomputer %COMPUTERNAME% /newname:SRV1
 
+###########
+# Firewall
+
+#netsh advfirewall set allprofiles state off
+###LOG
+##netsh advfirewall set currentprofile logging filename %systemroot%\system32\LogFiles\Firewall\pfirewall.log
+#netsh advfirewall set currentprofile logging maxfilesize 4096
+#netsh advfirewall set currentprofile logging droppedconnections enable
+##netsh advfirewall set currentprofile logging allowedconnections enable
+
+netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=yes
+netsh advfirewall firewall set rule name="File and Printer Sharing (SMB-In)" dir=in new enable=Yes
+########
+
 ::onlinerun https://raw.githubusercontent.com/vinils/baxh/master/w2k19/DriversNetWork.bat
 ::onlinerun https://raw.githubusercontent.com/vinils/baxh/master/w2k19/DriversNetworkAfterReboot.ps1
 ::onlinerun https://raw.githubusercontent.com/vinils/baxh/master/w2k19/EnableRemoteDesktop.ps1
