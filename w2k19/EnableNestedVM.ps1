c5380c77064318aa2ff9f2ea31c8029e595e7195
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-29 02:32:31 -0300

    Update EnableNestedVM.ps1

--- w2k19/EnableNestedVM.ps1
+++ w2k19/EnableNestedVM.ps1
@@ -1,7 +1,11 @@
-###########
-#NESTED 
+[CmdletBinding()]
+param(
+        $Name,
+        [System.Management.Automation.PSCredential]
+        $Credential = $(Get-Credential)
+    )
 
-#Get-VMProcessor -VMName $args[0] | fl *
+#Get-VMProcessor -VMName $Name | fl *
 
 If($args[1]) {
   $bool = $true
@@ -11,15 +15,15 @@ If($args[1]) {
   $OnOff = 'Off'
 }
 
-Set-VMProcessor -VMName $args[0] -ExposeVirtualizationExtensions $bool
-#(Get-VMProcessor -VMName $args[0]).ExposeVirtualizationExtensions
+Set-VMProcessor -VMName $Name -ExposeVirtualizationExtensions $bool
+#(Get-VMProcessor -VMName $Name).ExposeVirtualizationExtensions
 #disable Dynamic Memory on Virtual Machine
-Set-VMMemory $args[0] -DynamicMemoryEnabled $bool
-#(Get-VMMemory -VMName $args[0]).DynamicMemoryEnabled
+Set-VMMemory $Name -DynamicMemoryEnabled $bool
+#(Get-VMMemory -VMName $Name).DynamicMemoryEnabled
 #Enable mac address spoofing
-Get-VMNetworkAdapter -VMname $args[0] | Set-VMNetworkAdapter -MacAddressSpoofing $OnOff
-#(Get-VMNetworkAdapter -VMName $args[0]).MacAddressSpoofing
+Get-VMNetworkAdapter -VMname $Name | Set-VMNetworkAdapter -MacAddressSpoofing $OnOff
+#(Get-VMNetworkAdapter -VMName $Name).MacAddressSpoofing
 
-Wait-VMPowershell -Name $args[0] -Credential
+Wait-VMPowershell -Name $Name -Credential $Credential
 
-#Invoke-Command -VMName $args[0] -ScriptBlock { Get-Service }
+Invoke-Command -VMName $Name -ScriptBlock { Get-Service }
