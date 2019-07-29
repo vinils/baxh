ccd332805800e6a9413e4732c0476151601955ac
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-29 02:27:37 -0300

    Create Wait-VMPowershell.ps1

---
+++ Wait-VMPowershell.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param(
+        $Name,
+        [System.Management.Automation.PSCredential]
+	$Credential = $(Get-Credential)
+    )
+
+# Turn on virtual machine if it is not running
+If ((Get-VM -VMName $Name).State -eq "Off")
+{
+  Write-Host "Starting VM $($Name)"
+  Start-VM -Name $Name -ErrorAction Stop | Out-Null
+}
+
+Wait-VM -Name $Name -For Heartbeat
+#Start-Sleep -Seconds 20
+
+$startTime = Get-Date
+do 
+{
+  $timeElapsed = $(Get-Date) - $startTime
+  if ($($timeElapsed).TotalMinutes -ge 10)
+  {
+      Write-Host "Could not connect to PS Direct after 10 minutes"
+      throw
+  } 
+  Start-Sleep -sec 1
+  $psReady = Invoke-Command -VMName $Name -Credential $Credential `
+                            -ScriptBlock { $True } -ErrorAction SilentlyContinue
+} 
+until ($psReady)
