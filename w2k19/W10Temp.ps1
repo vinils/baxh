c6df4b11e6c2dad1bed47b3fe17d7af86881fae7
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-07-31 11:58:35 -0300

    Update W10Temp.ps1

--- w2k19/W10Temp.ps1
+++ w2k19/W10Temp.ps1
@@ -37,10 +37,30 @@ Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Modu
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Get-WindowsUpdate }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-WindowsUpdate -AcceptAll -IgnoreReboot }
 
-$Write-Host "Unpin"
-$Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Pin-App "Store" -unpin }
-$Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Pin-App "Microsoft Edge" -unpin }
-$Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Pin-App "Mail" -unpin }
+#removing mail app
+#Write-Host "Removing mail app"
+#Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { get-appxpackage *microsoft.windowscommunicationsapps* | remove-appxpackage }
+
+Write-Host "removing microsoft edge link from desktop"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { del "C:\Users\MyUser\Desktop\Microsoft Edge.lnk" }
+Write-Host "unpin microsoft edge"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq "Microsoft Edge"}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true} }
+Write-Host "unpin store"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq "Microsoft Store"}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true} }
+Write-Host "unpin mail"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq "Mail"}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true} }
+Write-Host "install chocolatey"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+Write-Host "install NotepePlusPlus"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { choco install -y --limit-output --no-progress NotepadPlusPlus }
+Write-Host "install 7zip"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { choco install -y --limit-output --no-progress 7zip }
+Write-Host "install GoogleChrome"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { choco install -y --limit-output --no-progress GoogleChrome }
+Write-Host "pin chrome"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq "Google Chrome"}).Verbs() | ?{$_.Name.replace('&','') -match 'To "Start" Pin|Pin to Start'} | %{$_.DoIt()} }
+Write-Host "removing microsoft chrome link from desktop"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { del "C:\Users\Public\Desktop\Google Chrome.lnk" }
 
 Write-Host "Renaming computer name"
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Rename-computer -computername $(HOSTNAME) -newname $using:Name }
