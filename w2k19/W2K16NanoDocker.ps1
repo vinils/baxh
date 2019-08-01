dc7c2cf039d69324798f54b778f67be8fb8cb74f
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-01 06:33:17 -0300

    Update W2K16NanoDocker.ps1

--- w2k19/W2K16NanoDocker.ps1
+++ w2k19/W2K16NanoDocker.ps1
@@ -1,4 +1,5 @@
 #https://docs.microsoft.com/pt-br/windows-server/get-started/deploy-nano-server
+#testing
 
 $Name = "W16Docker4"
 $isoPath = 'D:\SOFTWARES\WORK\MS Windows\2016 Server\14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_EN-US.ISO'
