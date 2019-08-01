15a6641405cf4bb266f50679d61b14467e9dff56
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-01 10:49:03 -0300

    Update W2K16NanoDocker.ps1

--- w2k19/W2K16NanoDocker.ps1
+++ w2k19/W2K16NanoDocker.ps1
@@ -65,8 +65,7 @@ Write-Host "Installing Docker"
 #Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Verbose }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Module DockerMsftProvider -Force }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package Docker -ProviderName DockerMsftProvider -Force }
-$docker = Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { try { Start-Service docker } catch { return $false } }
-$count=$count+1
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-Service docker }
 
 For ($i=1; $i -le 3; $i++) {
   #https://docs.microsoft.com/pt-br/windows-server/get-started/manage-nano-server
