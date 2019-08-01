4f4718808019b6e5baf0501746eaf33aa1475a40
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-01 06:57:04 -0300

    Update W2K16NanoDocker.ps1

--- w2k19/W2K16NanoDocker.ps1
+++ w2k19/W2K16NanoDocker.ps1
@@ -81,6 +81,7 @@ Write-Host "Installing Docker"
 #Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Verbose }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Module DockerMsftProvider -Force }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package Docker -ProviderName DockerMsftProvider -Force }
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-Service docker }
 
 Write-Host "Restarting VM"
 Restart-VM $Name -Force
