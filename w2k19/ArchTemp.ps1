2bf2e28218b91ce71a4dcba8d6b22df6c90f2806
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-03 06:02:34 -0300

    Update ArchTemp.ps1

--- w2k19/ArchTemp.ps1
+++ w2k19/ArchTemp.ps1
@@ -1,10 +1,15 @@
 $Name = 'ArchTemp'
 New-VHD -Path "E:\Hyper-V\Virtual Hard Disks\$Name.vhdx" -SizeBytes 100GB -Dynamic -BlockSizeBytes 1MB
 New-VM -Name $Name -MemoryStartupBytes 10GB -VHDPath "E:\Hyper-V\Virtual Hard Disks\$Name.vhdx" -Generation 2 -SwitchName ExternalSwitch
+
+SET-VMProcessor –VMName $Name –Count 2
+Set-VMMemory -VMName $Name -DynamicMemoryEnabled $true -StartupBytes 2GB
+
 Add-VMScsiController -VMName $Name
 Add-VMDvdDrive -VMName $Name -ControllerNumber 1 -ControllerLocation 0 -Path 'D:\SOFTWARES\WORK\Linux\archlinux-2019.07.01-x86_64.iso'
 $DVDDrive = Get-VMDvdDrive -VMName $Name
 Set-VMFirmware -VMName $Name -EnableSecureBoot Off -FirstBootDevice $DVDDrive
+
 Start-VM -Name $Name
 
 Write-Host "Waiting you to install Arch"
