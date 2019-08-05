1b939deb71be117a00eed16e4b1c6a6497f7d1cb
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-05 17:54:16 -0300

    Create Node.bat

---
+++ w10/Node.bat
@@ -0,0 +1,16 @@
+#VSCode
+choco install vscode -y --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders"
+
+#choco install nodejs.install
+
+#NodeJs throw NVM
+choco install -y nvm
+nvm install latest
+#nvm use 12.1.0 64
+
+#Yarn
+choco install -y yarn
+
+##gulp
+#choco install -y gulp-cli
+
