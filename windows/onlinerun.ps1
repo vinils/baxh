68ffc622bbe0f229b959197e776326416dd97e5f
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-05 21:58:05 -0300

    Update onlinerun.ps1

--- windows/onlinerun.ps1
+++ windows/onlinerun.ps1
@@ -1,2 +1,2 @@
 $command = ((new-object net.webclient).DownloadString($args[0]))
-Invoke-Command -ScriptBlock ([scriptblock]::Create($command)) -Argumentlist $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7], $args[8], $args[9], $args[10], $args[11]
+Invoke-Command -ScriptBlock ([scriptblock]::Create($command)) -Argumentlist @args
