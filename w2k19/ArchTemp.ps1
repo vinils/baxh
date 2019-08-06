ac3b3eae2a7b2c5608b24015d8a450eee72f06a3
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-06 12:59:13 -0300

    Update ArchTemp.ps1

--- w2k19/ArchTemp.ps1
+++ w2k19/ArchTemp.ps1
@@ -2,13 +2,13 @@ $Name = 'ArchTemp'
 New-VHD -Path "E:\Hyper-V\Virtual Hard Disks\$Name.vhdx" -SizeBytes 100GB -Dynamic -BlockSizeBytes 1MB
 New-VM -Name $Name -MemoryStartupBytes 10GB -VHDPath "E:\Hyper-V\Virtual Hard Disks\$Name.vhdx" -Generation 2 -SwitchName ExternalSwitch
 
-SET-VMProcessor –VMName $Name –Count 2
-Set-VMMemory -VMName $Name -DynamicMemoryEnabled $true -StartupBytes 2GB
-
 Add-VMScsiController -VMName $Name
 Add-VMDvdDrive -VMName $Name -ControllerNumber 1 -ControllerLocation 0 -Path 'Z:\SOFTWARES\WORK\Linux\archlinux-2019.07.01-x86_64.iso'
 $DVDDrive = Get-VMDvdDrive -VMName $Name
 Set-VMFirmware -VMName $Name -EnableSecureBoot Off -FirstBootDevice $DVDDrive
+
+Set-VMMemory -VMName $Name -DynamicMemoryEnabled $true -StartupBytes 2GB
+Set-VMProcessor -VMName $Name -Count 2
 
 Start-VM -Name $Name
 
