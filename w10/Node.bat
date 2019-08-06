4371a187ab25cc35339daa8e48d33a44d91cac98
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-05 22:30:53 -0300

    Update Node.bat

--- w10/Node.bat
+++ w10/Node.bat
@@ -1,16 +1,16 @@
 #VSCode
-choco install vscode -y --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders"
+choco install vscode -y --limit-output --no-progress --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders"
 
 #choco install nodejs.install
 
 #NodeJs throw NVM
-choco install -y nvm
+choco install -y --limit-output --no-progress nvm
 nvm install latest
 #nvm use 12.1.0 64
 
 #Yarn
-choco install -y yarn
+choco install -y --limit-output --no-progress yarn
 
 ##gulp
-#choco install -y gulp-cli
+#choco install -y --limit-output --no-progress gulp-cli
 
