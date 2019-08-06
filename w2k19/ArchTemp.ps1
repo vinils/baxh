5eb71cc9ea4240af90a108695fb3bb542f4803be
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-06 12:57:37 -0300

    Update ArchTemp.ps1

--- w2k19/ArchTemp.ps1
+++ w2k19/ArchTemp.ps1
@@ -6,7 +6,7 @@ SET-VMProcessor –VMName $Name –Count 2
 Set-VMMemory -VMName $Name -DynamicMemoryEnabled $true -StartupBytes 2GB
 
 Add-VMScsiController -VMName $Name
-Add-VMDvdDrive -VMName $Name -ControllerNumber 1 -ControllerLocation 0 -Path 'D:\SOFTWARES\WORK\Linux\archlinux-2019.07.01-x86_64.iso'
+Add-VMDvdDrive -VMName $Name -ControllerNumber 1 -ControllerLocation 0 -Path 'Z:\SOFTWARES\WORK\Linux\archlinux-2019.07.01-x86_64.iso'
 $DVDDrive = Get-VMDvdDrive -VMName $Name
 Set-VMFirmware -VMName $Name -EnableSecureBoot Off -FirstBootDevice $DVDDrive
 
