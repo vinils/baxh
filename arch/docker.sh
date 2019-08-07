06c2850dc19c3e83f262b6e05cc9822852ed628e
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-07 20:59:41 -0300

    Update docker.sh

--- arch/docker.sh
+++ arch/docker.sh
@@ -10,3 +10,6 @@ eval "$(ssh-agent -s)"
 eval "$(ssh-add ~/.ssh/id_rsa)"
 docker-machine create --driver generic --generic-ip-address=192.168.15.31 --generic-ssh-key ~/.ssh/id_rsa --generic-ssh-user=myuser --generic-ssh-port=22 ArchDocker2
 docker-machine regenerate-certs ArchDocker2
+
+eval $(docker-machine env ArchDocker2)
+eval $(docker-machine env -u)
