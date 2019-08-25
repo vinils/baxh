e85ceede824df6f54072a9608affbf34d88ecf42
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-25 15:18:54 -0300

    Update Docker.ps1

--- w2k19/Docker.ps1
+++ w2k19/Docker.ps1
@@ -3,7 +3,7 @@ Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Modu
 ##Bug Cannot verify the file SHA256 - problem to download file
 #Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package Docker -ProviderName DockerMsftProvider -Force }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider }
-Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-BitsTransfer -Source https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -Destination C:\Users\ADMINI~1\AppData\Local\Temp\DockerMsftProvider\Docker-19-03-1.zip }
-Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-BitsTransfer -Source https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -Destination C:\Users\ADMINI~1\AppData\Local\Temp\2\DockerMsftProvider\Docker-19-03-1.zip }
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Invoke-WebRequest https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -OutFile C:\Users\ADMINI~1\AppData\Local\Temp\DockerMsftProvider\Docker-19-03-1.zip }
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Invoke-WebRequest https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -OutFile C:\Users\ADMINI~1\AppData\Local\Temp\2\DockerMsftProvider\Docker-19-03-1.zip }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-Service docker }
