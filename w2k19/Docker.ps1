3829517ae715c2ed77299c567ce32e5a1acc073a
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-25 14:11:58 -0300

    Update Docker.ps1

--- w2k19/Docker.ps1
+++ w2k19/Docker.ps1
@@ -1,16 +1,7 @@
 Write-Host "Installing Docker"
-#Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Module -Name DockerMsftProvider -Repository PSGallery -Force }
-#Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Verbose }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Module DockerMsftProvider -Force }
 ##Bug Cannot verify the file SHA256 - problem to download file
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-BitsTransfer -Source https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -Destination C:\Users\ADMINI~1\AppData\Local\Temp\2\DockerMsftProvider\Docker-19-03-1.zip }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package -Name docker -ProviderName DockerMsftProvider }
 #Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Install-Package Docker -ProviderName DockerMsftProvider -Force }
 Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Start-Service docker }
-
-$isRebootPending = Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { return (Test-PendingReboot).IsRebootPending }
-if($isRebootPending) {
-  Write-Host "Restarting VM"
-  Restart-VM $Name -Force
-  Wait-VMPowershell -Name $Name -Credential $Credential
-}
