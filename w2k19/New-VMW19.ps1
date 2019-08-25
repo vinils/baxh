fec911120af8b7ebec9b7805e338f4f7484f3ab6
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-25 12:05:22 -0300

    Create New-VMW19.ps1

---
+++ w2k19/New-VMW19.ps1
@@ -0,0 +1,41 @@
+$baseName = 'W19Temp'
+$Name = $args[0]
+$DriveOpt = $args[1]
+$UriSettings = $args[2]
+
+switch($DriveOpt) {
+   1 { $drive = 'D:' }
+   2 { $drive = 'E:' }
+   3 { $drive = 'F:' }
+   4 { $drive = 'G:' }
+   5 { $drive = 'H:' }
+   6 { $drive = 'Z:' }
+}
+
+$filePath = "$drive\Hyper-V\Virtual hard disks\$Name.vhdx"
+copy "E:\Hyper-V\Virtual hard disks\$baseName.vhdx" $filePath
+
+New-VM -Name $Name `
+  -VHDPath $filePath `
+  -MemoryStartupBytes 10GB `
+  -Generation 2 `
+  -SwitchName ExternalSwitch
+
+Set-VMMemory -VMName $Name -DynamicMemoryEnabled $true -StartupBytes 8GB
+Set-VMProcessor -VMName $Name -Count 2
+
+$Credential = New-Object System.Management.Automation.PSCredential ("MyUser", (new-object System.Security.SecureString))
+Wait-VMPowershell -Name $Name -Credential $Credential
+
+if($UriSettings -ne $null) { 
+  Write-Host "Installing custom configurations"
+  Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { onlinerun $using:UriSettings }
+}
+
+Write-Host "Renaming computer name"
+Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { Rename-computer -computername $(HOSTNAME) -newname $using:Name }
+
+Restart-VM $Name -Force
+Wait-VMPowershell -Name $Name -Credential $Credential
+
+Write-Host $(Invoke-Command -VMName $Name -Credential $Credential -ScriptBlock { ipconfig | findstr /i "ipv4" })
