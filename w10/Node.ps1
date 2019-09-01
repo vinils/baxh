9e98164636451324393afcb5a79073f0a2f84c5a
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2019-08-31 22:34:40 -0300

    Update Node.ps1

--- w10/Node.ps1
+++ w10/Node.ps1
@@ -1,14 +1,16 @@
 #VSCode
 choco install vscode -y --limit-output --no-progress --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders"
 
+##old way
 #choco install nodejs.install
 
-#NodeJs throw NVM
-choco install -y --limit-output --no-progress nvm
-start powershell -wait {nvm install latest}
-#nvm ls
-#nvm use 12.7.0
-start powershell -wait {nvm use $(nvm ls)}
+#bug problem using nvm through choco - next time try invoke-webrequest nvm release page "https://github.com/coreybutler/nvm-windows/releases" unzip nvm and nvm-setup.exe /?
+##NodeJs throw NVM
+#choco install -y --limit-output --no-progress nvm
+#start powershell -wait {nvm install latest}
+##nvm ls
+##nvm use 12.7.0
+#start powershell -wait {nvm use $(nvm ls)}
 
 #Yarn
 choco install -y --limit-output --no-progress yarn
