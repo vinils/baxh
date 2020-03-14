275506408609cdd5ad5a4186fd9e9e99c1a26539
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2020-03-14 13:39:43 -0300

    Update w10.psm1

--- w10/w10.psm1
+++ w10/w10.psm1
@@ -617,3 +617,44 @@ function DoPin([string]$appname){
     ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object {$_.Name -eq $appname}).Verbs() | Where-Object {$_.Name.replace('&','') -match 'Pin to Taskbar'} |  ForEach-Object {$_.DoIt();}
     $ErrorActionPreference= 'continue'
 }
+
+Function Extend-WinOSDiskSize {
+	foreach($disk in Get-Disk)
+	{
+		# Check if the disk in context is a Boot and System disk
+		if((Get-Disk -Number $disk.number).IsBoot -And (Get-Disk -Number $disk.number).IsSystem)
+		{
+			# Get the drive letter assigned to the disk partition where OS is installed
+			$driveLetter = (Get-Partition -DiskNumber $disk.Number | where {$_.DriveLetter}).DriveLetter
+			Write-verbose "Current OS Drive: $driveLetter :\"
+
+			# Get current size of the OS parition on the Disk
+			$currentOSDiskSize = (Get-Partition -DriveLetter $driveLetter).Size        
+			Write-verbose "Current OS Partition Size: $currentOSDiskSize"
+
+			# Get Partition Number of the OS partition on the Disk
+			$partitionNum = (Get-Partition -DriveLetter $driveLetter).PartitionNumber
+			Write-verbose "Current OS Partition Number: $partitionNum"
+
+			# Get the available unallocated disk space size
+			$unallocatedDiskSize = (Get-Disk -Number $disk.number).LargestFreeExtent
+			Write-verbose "Total Unallocated Space Available: $unallocatedDiskSize"
+
+			# Get the max allowed size for the OS Partition on the disk
+			$allowedSize = (Get-PartitionSupportedSize -DiskNumber $disk.Number -PartitionNumber $partitionNum).SizeMax
+			Write-verbose "Total Partition Size allowed: $allowedSize"
+
+			if ($unallocatedDiskSize -gt 0 -And $unallocatedDiskSize -le $allowedSize)
+			{
+				$totalDiskSize = $allowedSize
+				
+				# Resize the OS Partition to Include the entire Unallocated disk space
+				$resizeOp = Resize-Partition -DriveLetter C -Size $totalDiskSize
+				Write-verbose "OS Drive Resize Completed $resizeOp"
+			}
+			else {
+				Write-Verbose "There is no Unallocated space to extend OS Drive Partition size"
+			}
+		}   
+	}
+}
